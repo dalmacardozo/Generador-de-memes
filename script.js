4cedd714437c04ea4734ed1b926d34f91238ff42
dalmacardozo <dalmam.cardozo@gmail.com>
2023-07-03 22:16:27 -0300

    Se realiza funcionalidad del input URL e input range

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ const botonImg = document.getElementById('boton-img');
 const botonText = document.getElementById('boton-text');
 const asideImg = document.getElementById('aside-img');
 const asideText = document.getElementById('aside-text');
-
 const botonModo = document.getElementById('boton-modo');
 const botonModoClaro = document.getElementById('boton-modo')
 
@@ -23,7 +22,7 @@ const modoClaro = () => {
 
 botonModoClaro.addEventListener('click', () => modoClaro())
 
-//Displey Hidden en columnas asaid
+//Displey oculto en columnas asaid
 
 const ocultoAsideImg = () => {
     asideText.classList.add('oculto');
@@ -39,16 +38,54 @@ const ocultoAsideText = () => {
 botonImg.addEventListener('click', ()=> ocultoAsideImg())
 botonText.addEventListener('click', ()=> ocultoAsideText())
 
+//URL-IMAGEN
 
-//Input color en columna IMG
+const inputUrl = document.getElementById('input-img')
+const imgUrl = document.getElementById('meme-img')
 
-const contenedorMeme = document.getElementById('meme-fondo');
-const inputColor = document.getElementsById('opciones-de-color')
+const changeBackground = (e) => {
+    imgUrl.style.backgroundImage = `url('${e.target.value}')`
+ }
 
+inputUrl.addEventListener('input', (e) => changeBackground(e))
 
-const cambioDeColor = (color) => {
-    contenedorMeme.style.backgroundColor = (color)
+//INPUT RANGE
+
+const brillo = document.getElementById('brillo-input')
+console.log(brillo)
+const opacidad = document.getElementById('opacidad-input')
+const contraste = document.getElementById('contraste-input')
+const desenfoque = document.getElementById('desenfoque-input')
+const grises = document.getElementById('grises-input')
+const sepia = document.getElementById('sepia-input')
+const hue = document.getElementById('hue-input')
+const negativo = document.getElementById('negativo-input')
+const saturado = document.getElementById('saturado-input')
+
+const filtros = (e) => {
+    console.log(e.target.value)
+    imgUrl.style.filter = `brightness(${brillo.value}) opacity(${opacidad.value}) blur(${desenfoque.value}px) contrast(${contraste.value}%) grayscale(${grises.value}%) hue-rotate(${hue.value}deg) sepia(${sepia.value}%) saturate(${saturado.value}%) invert(${negativo.value})`
 
 }
 
-inputColor.addEventListener('click', (evento) => cambioDeColor(evento))
+brillo.addEventListener('input', (e) => filtros(e))
+opacidad.addEventListener('input', (e) => filtros(e))
+contraste.addEventListener('input', (e) => filtros(e)) 
+desenfoque.addEventListener('input', (e) => filtros(e))
+grises.addEventListener('input', (e) => filtros(e))
+sepia.addEventListener('input', (e) => filtros(e))
+hue.addEventListener('input', (e) => filtros(e))
+negativo.addEventListener('input', (e) => filtros(e))
+saturado.addEventListener('input', (e) => filtros(e))
+
+
+//INPUT COLOR
+
+//const inputColor = document.getElementsById('opciones-de-color')
+
+
+//const cambioDeColor = (c)  => {
+    //imgUrl.style.backgroundColor = `('${c.target.value}')`}
+
+
+//inputColor.addEventListener('input', (color) => cambioDeColor(color))
